8a82de7d7aeafcbbc5f7071eb274ccaaf4a13409
teamcipx <hello.alihosen@gmail.com>
2026-01-16 16:57:13 +0600

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, ImageMetadata } from './types';
+import { BrowserRouter, View, ImageMetadata } from './types';
 import { ADMIN_SECRET_PATH } from './constants';
 import { storageService } from './services/storageService';
 import Navbar from './components/Navbar';
